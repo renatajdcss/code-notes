6aa753b83d2cca666ed4d025ffc9aaf44a47ac44
Renata Sanches <renatajdcss@users.noreply.github.com>
2020-07-05 16:17:45 -0400

    config: update description and fix path

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: 'Renata\'s Code notes',
-    description: `Quick notes I can reference to later.`
+    description: `Quick notes and snippets I can reference to later.`
   },
   pathPrefix: "/code-notes",
   plugins: [
@@ -12,7 +12,7 @@ module.exports = {
         basePath: '/',
         showThemeInfo: true,
         showDescriptionInSidebar: true,
-        gitRepoContentPath: 'https://github.com/renatajdcss/code-notes',
+        gitRepoContentPath: 'https://github.com/renatajdcss/code-notes/',
         logo: 'https://avatars2.githubusercontent.com/u/12951469?s=460&u=6dd26b0dc82ea2277ea7660d67f4634c3092803e&v=4'
       },
     },
